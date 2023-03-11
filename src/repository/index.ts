2aba6a55a3cb2009168dbaaea994a04045612662
victorraffaele <victorraffaele@gmail.com>
2023-03-11 02:09:48 -0300

    fix: comment test function

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -13,17 +13,17 @@ const userRep = new UserRepository();
 // }
 // testUser();
 
-async function testPost(){
-    const postRep = new PostRepository();
-    //const t: iResp = await postRep.insert({media: '../../img/download.png', authorId: '6e3b98db-961e-47c8-be9a-84164341af98', author: 'test'})
-    const t: iResp = await postRep.listAll();
-    //const t: iResp = await postRep.listBy({key :"b05a8601-6c4e-4eea-906c-f70197c28351"}) 
-    //const t: iResp = await postRep.like({postKey: "b05a8601-6c4e-4eea-906c-f70197c28351", userKey: "6e3b98db-961e-47c8-be9a-84164341af98"})
-    //const t: iResp = await postRep.unlike({postKey: "b05a8601-6c4e-4eea-906c-f70197c28351", userKey: "6e3b98db-961e-47c8-be9a-84164341af98"})
-    //const t: iResp = await postRep.comment({postKey: "b05a8601-6c4e-4eea-906c-f70197c28351", comment: "test2"})
-    console.log(t)
-}
-testPost();
+// async function testPost(){
+//     const postRep = new PostRepository();
+//     //const t: iResp = await postRep.insert({media: '../../img/download.png', authorId: '6e3b98db-961e-47c8-be9a-84164341af98', author: 'test'})
+//     const t: iResp = await postRep.listAll();
+//     //const t: iResp = await postRep.listBy({key :"b05a8601-6c4e-4eea-906c-f70197c28351"}) 
+//     //const t: iResp = await postRep.like({postKey: "b05a8601-6c4e-4eea-906c-f70197c28351", userKey: "6e3b98db-961e-47c8-be9a-84164341af98"})
+//     //const t: iResp = await postRep.unlike({postKey: "b05a8601-6c4e-4eea-906c-f70197c28351", userKey: "6e3b98db-961e-47c8-be9a-84164341af98"})
+//     //const t: iResp = await postRep.comment({postKey: "b05a8601-6c4e-4eea-906c-f70197c28351", comment: "test2"})
+//     console.log(t)
+// }
+// testPost();
 
 // async function testComment(){
     
