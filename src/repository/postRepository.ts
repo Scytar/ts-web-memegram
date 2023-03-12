06ad6ab9e979334216a5d85802392df411b546d9
victorraffaele <victorraffaele@gmail.com>
2023-03-12 00:37:35 -0300

    refactor: reduced repository's duplicate code and private some variables

--- src/repository/postRepository.ts
+++ src/repository/postRepository.ts
@@ -6,146 +6,108 @@ import iResp from "../interfaces/iResp";
 dotenv.config();
 const MONGODB_DSN = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PSW}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
-const postSchem = new mongoose.Schema({
-    key: {
-        type: String,
-        unique: true
-    },
-    post: {
-        authorId: {
-            type: String,
-            required: true
-        },
-        author: {
-            type: String,
-            required: true
-        },
-        media: {
-            type: String,
-            required: true,
-        },
-        likes: Array<String>,
-        comments: Array<any>
-    },
-    created_at: Date,
-})
-
-const postModel = mongoose.model('post', postSchem);
-
 export default class PostRepository{
-    async insert(postData: any){
-        //Receive an Object with the data to insert
+    private postSchem = new mongoose.Schema({
+        key: {
+            type: String,
+            unique: true
+        },
+        post: {
+            authorId: {
+                type: String,
+                required: true
+            },
+            author: {
+                type: String,
+                required: true
+            },
+            media: {
+                type: String,
+                required: true,
+            },
+            likes: Array<String>,
+            comments: Array<any>
+        },
+        created_at: Date,
+    })
+    
+    private postModel = mongoose.model('post', this.postSchem);
+
+    private async base(resultFunc){
+        //Receive an function, make conection with database and run the operation
         let resp: iResp = {data: null, error: null};
         try {
             await mongoose.connect(MONGODB_DSN);
-            const newPost = new postModel({
-                key: uuid(),
-                post: {      
-                    authorId: postData.authorId,
-                    author: postData.author,
-                    media: postData.media,
-                    likes: [],
-                    comments:[]
-                },
-                created_at: Date.now()
-            });
-            const result: any = await newPost.save()
-            if(result){
-                resp.data = `Postagem realizada com sucesso!`;
-            };
+            console.log('abriu')
+            resp.data = await resultFunc;
             await mongoose.connection.close();
+            console.log('fechoou')
         } catch (err: any) {
             resp.error = err.message;
         }
         return resp;
     }
+
+    async insert(postData: any){
+        //Receive an Object with the data to insert
+        const newPost = new this.postModel({
+            key: uuid(),
+            post: {      
+                authorId: postData.authorId,
+                author: postData.author,
+                media: postData.media,
+                likes: [],
+                comments:[]
+            },
+            created_at: Date.now()
+        });
+        const resp:iResp = await this.base(newPost.save())
+        return resp;
+    }
     
     async listAll() {
-        let resp: iResp = {data: null, error: null};
-        try {
-            await mongoose.connect(MONGODB_DSN);
-            const result: any = await postModel.find();
-            if (result) {
-                resp.data = result;
-            }
-            await mongoose.connection.close();
-        } catch (err: any) {
-            resp.error = err.message;
-        }
+        const resp: iResp = await this.base(this.postModel.find());
         return resp;
     }
     
     async listBy(query: any) { 
         //Receive an Object with the filter of search
-        let resp: iResp = {data: null, error: null};
-        try {
-            await mongoose.connect(MONGODB_DSN);
-            const result: any = await postModel.find(query);
-            if (result) {
-                resp.data = result;
-            }
-            await mongoose.connection.close();
-        } catch (err: any) {
-            resp.error = err.message;
+        const resp: iResp = await this.base(this.postModel.find(query));
+        if (resp.data[0] == null) {
+            resp.error = "Error: not found" 
         }
         return resp;
     }
 
     async like(postData: any){
-        let resp: iResp = {data: null, error: null};
-        try {
+        //Add or remove a like based on user key
+        let resp: iResp = await this.base(this.postModel.findOne(
+            {key: postData.postKey}
+        ));
+        if (!resp.data.post.likes.includes(postData.userKey)) {
             await mongoose.connect(MONGODB_DSN);
-            const result = await postModel.findOne(
-                {key: postData.postKey}
-            );
-            if (result) {
-                result?.post?.likes?.push(postData.userKey);
-                await result.save();
-                resp.data = 'Liked';
-             }
-            await mongoose.connection.close();
-        } catch (err) {
-            resp.error = err.message;
-        }
-        return resp;
-    }
-
-    async unlike(postData: any){
-        let resp: iResp = {data: null, error: null};
-        try {
+            resp.data.post.likes.push(postData.userKey);
+            console.log(resp.data)
+            resp = await this.base(resp.data.save());
+        } else{
             await mongoose.connect(MONGODB_DSN);
-            const result = await postModel.findOne(
-                {key: postData.postKey}
-            );
-            if (result) {
-                const index = result?.post?.likes?.indexOf(postData.userKey);
-                result?.post?.likes?.splice(index, 1);
-                await result.save();
-                resp.data = 'Unliked';
-            }
-            await mongoose.connection.close();
-        } catch (err) {
-            resp.error = err.message;
+            const index = resp.data.post.likes.indexOf(postData.userKey);
+            resp.data.post.likes.splice(index, 1);
+            resp = await this.base(resp.data.save());
         }
         return resp;
     }
 
     async comment(postData: any){
-        let resp: iResp = {data: null, error: null};
-        try{
-            await mongoose.connect(MONGODB_DSN);
-            const result = await postModel.findOne(
+        //Add a comment in a post
+        let resp: iResp = await this.base(this.postModel.findOne(
                 {key: postData.postKey}
-            );
-            if (result) {
-                result?.post?.comments?.push(postData.comment);
-                await result.save();
-                resp.data = 'Commented';
+            ));
+            if (resp.data) {
+                await mongoose.connect(MONGODB_DSN);
+                resp.data.post.comments.push(postData.comment);
+                resp = await this.base(resp.data.save());
             }
-            await mongoose.connection.close();
-        }catch (err) {
-            resp.error = err.message;
-        }
         return resp;
     }
 }
--- src/repository/userRepository.ts
+++ src/repository/userRepository.ts
@@ -6,124 +6,104 @@ import iResp from "../interfaces/iResp";
 dotenv.config();
 const MONGODB_DSN = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PSW}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
 
-const userSchem = new mongoose.Schema({
-    key: {
-        type: String,
-        unique: true
-    },
-    user: {
-        name: {
+export default class UserRepository{
+    private userSchem = new mongoose.Schema({
+        key: {
             type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true,
             unique: true
         },
-        password: {
-            type: String,
-            required: true
+        user: {
+            name: {
+                type: String,
+                required: true
+            },
+            email: {
+                type: String,
+                required: true,
+                unique: true
+            },
+            password: {
+                type: String,
+                required: true
+            },
         },
-    },
-    created_at: Date,
-    updated_at: Date,
-    deleted_at: Date
-})
+        created_at: Date,
+        updated_at: Date,
+        deleted_at: Date
+    })
+    
+    private userModel = mongoose.model('user', this.userSchem);
 
-const userModel = mongoose.model('user', userSchem);
-
-export default class UserRepository{
-    async insert(userData: any){
-        //Receive an Object with the data to insert
+    private async base(resultFunc){
+        //Receive an function, make conection with database and run the operation
         let resp: iResp = {data: null, error: null};
         try {
             await mongoose.connect(MONGODB_DSN);
-            const newUser = new userModel({
-                key: uuid(),
-                user:{
-                    name: userData.name,
-                    email: userData.email,
-                    password: userData.password
-                },
-                created_at: Date.now(),
-                updated_at: null,
-                deleted_at: null
-            });
-            const result: any = await newUser.save()
-            if(result){
-                resp.data = `Usuario ${result.user.name} cadastrado com sucesso!`;
-            }
+            resp.data = await resultFunc;
             await mongoose.connection.close();
         } catch (err: any) {
             resp.error = err.message;
+        }
+        return resp;
+    }
+
+    async insert(userData: any){
+        //Receive an Object with the data to insert
+        const newUser = new this.userModel({
+            key: uuid(),
+            user:{
+                name: userData.name,
+                email: userData.email,
+                password: userData.password
+            },
+            created_at: Date.now(),
+            updated_at: null,
+            deleted_at: null
+        });
+        const resp: iResp = await this.base(newUser.save())
+        if(resp.data){
+            resp.data = `Usuario ${resp.data.user.name} cadastrado com sucesso!`;
         }
         return resp;
     }
     
     async listAll() {
-        let resp: iResp = {data: null, error: null};
-        try {
-            await mongoose.connect(MONGODB_DSN);
-            const result: any = await userModel.find();
-            if (result) {
-                resp.data = result;
-            }
-            await mongoose.connection.close();
-        } catch (err: any) {
-            resp.error = err.message;
-        }
+        const resp: iResp = await this.base(this.userModel.find());
         return resp;
     }
     
     async listBy(query: any) { 
         //Receive an Object with the filter of search
-        let resp: iResp = {data: null, error: null};
-        try {
-            await mongoose.connect(MONGODB_DSN);
-            const result: any = await userModel.find(query);
-            if (result) {
-                resp.data = result;
-            }
-            await mongoose.connection.close();
-        } catch (err: any) {
-            resp.error = err.message;
+        const resp: iResp = await this.base(this.userModel.find(query));
+        if (resp.data[0] == null) {
+           resp.error = "Error: not found" 
         }
         return resp;
     }
     
-    async update(updateData: any){ 
-        //Receive an Object with the filter of search(query) and the data to update(content)
-        let resp: iResp = {data: null, error: null};
-        try {
-            await mongoose.connect(MONGODB_DSN);
-            updateData.content.updated_at = Date.now();
-            const result: any = await userModel.findOneAndUpdate(
-                updateData.query, 
-                updateData.content,
-                {new: true});
-            if (result) {
-                resp.data = result;
-            }
-            await mongoose.connection.close();
-        } catch (err: any) {
-            resp.error = err.message;
+    async update(updateData: any){
+        //Receive an Object with the query to search the document and the data to update
+        updateData.content.updated_at = Date.now();
+        const resp: iResp = await this.base(this.userModel.findOneAndUpdate(
+            updateData.query, 
+            updateData.content,
+            {new: true}
+        ));
+        if (!resp.data) {
+            resp.error = "Error: not found";
         }
         return resp;
     }
     
     async deleteBy(deleteData: any){ 
         //Receive an Object with the filter of search(query) and try delete
-        let resp: iResp = {data: null, error: null};
-        try {
-            await mongoose.connect(MONGODB_DSN);
-            const result: any = await userModel.findOneAndDelete(deleteData.query, {new: true});
-            if (result) {
-                resp.data = `Usuario ${result.user.name} / ${result.user.email} foi deletado!`;
-            }
-            await mongoose.connection.close();
-        } catch (err: any) {
-            resp.error = err.message;
+        const resp: iResp = await this.base(this.userModel.findOneAndDelete(
+            deleteData.query, {new: true}
+        ));
+        if (resp.data) {
+            resp.data = `Usuario ${resp.data.user.name} / ${resp.data.user.email} foi deletado!`;
+        } else{
+            resp.error = "Error: not found";
         }
         return resp;
     }
