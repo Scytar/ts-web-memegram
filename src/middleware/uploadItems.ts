437dae6d60c178b1124354833d55d172a58daf3a
LuceliaFerreira <luceliatmf@gmail.com>
2023-03-15 01:53:37 -0300

    fix/multer

--- src/middleware/uploadItems.ts
+++ src/middleware/uploadItems.ts
@@ -1,28 +1,29 @@
-//Camada de autenticação do user com jsonwebtoken
 import { Response, Request } from 'express'
 import * as dotenv from 'dotenv';
 dotenv.config();
-import Multer from 'multer';
+import multer from 'multer';
 import path from "path";
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 
-const storage = Multer.diskStorage({
-    destination: "../uploads/",
-    filename: (req: Request, res: Response, file: Multer.File, cb: Multer.FileFilterCallback) => {
+const storage = multer.diskStorage({
+
+    destination: function (req: any, file: any, cb: any) {
+        cb(null, './uploads/')
+    },
+
+    filename: function (req: any, file: any, cb: any) {
         if (!file || !file.originalname) {
-           
             cb(null, "nochange");
-       
         } else {
-            
+
             const extension = path.extname(file.originalname);
             const filename = uuidv4() + extension;
             cb(null, filename);
-      
+
         }
-    },
+    }
 });
 
-const upload = Multer({ storage });
+const upload = multer({ storage });
 
 export default upload;
