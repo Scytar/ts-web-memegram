650c76f0b36093fa430996bede2b8af45df0122d
victorraffaele <victorraffaele@gmail.com>
2023-03-16 19:21:44 -0300

    feat: getting all post to feed

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import * as express from 'express';
 import { login, newUser } from '../controller/userController';
 import { newItems } from '../controller/postController';
 import { newComment, listComment } from '../controller/commentController';
+import { feed } from '../controller/feedController';
 import { authUser } from '../middleware/authUser';
 import upload from "../middleware/uploadItems";
 
@@ -21,6 +22,7 @@ router.post("/comment", authUser, newComment);
 router.get("/comment/:postId", authUser, listComment);
 
 //Auth - feed
+router.get("/feed", authUser, feed);
 //Auth - like
 
 
